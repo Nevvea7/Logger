7ae37a03f0407015187eddef8a385b0d7ff6b4cc
Anna Yan <annayen2008@gmail.com>
2016-06-19 21:30:20 -0700

    just adding some constructors

--- app/src/main/java/me/nevvea/logger/bean/LoggItem.java
+++ app/src/main/java/me/nevvea/logger/bean/LoggItem.java
@@ -1,6 +1,10 @@
 package me.nevvea.logger.bean;
 
+import android.database.Cursor;
+
 import java.sql.Time;
+
+import me.nevvea.logger.db.LoggDataHelper;
 
 /**
  * An object that holds a single log message
@@ -8,13 +12,34 @@ import java.sql.Time;
  * Created by Anna on 6/14/16.
  */
 public class LoggItem {
-    private int mDay;
-    private int mMonth;
-    private int mYear;
-    private Time mTime;
-    private String mMsg;
+    public int mDay;
+    public int mMonth;
+    public int mYear;
+    public Time mTime;
+    public String mMsg;
 
+    public LoggItem() {
 
+    }
+
+    public LoggItem(int day, int month, int year, Time time, String msg) {
+        this.mDay = day;
+        this.mMonth = month;
+        this.mYear = year;
+        this.mTime = time;
+        this.mMsg = msg;
+    }
+
+    public static LoggItem fromCursor(Cursor cursor) {
+        LoggItem loggItem = new LoggItem();
+        loggItem.mDay = cursor.getInt(cursor.getColumnIndex(LoggDataHelper.LoggDBInfo.COLUMN_LOG_DAY));
+        loggItem.mMonth = cursor.getInt(cursor.getColumnIndex(LoggDataHelper.LoggDBInfo.COLUMN_LOG_MONTH));
+        loggItem.mYear = cursor.getInt(cursor.getColumnIndex(LoggDataHelper.LoggDBInfo.COLUMN_LOG_YEAR));
+        int secs = cursor.getInt(cursor.getColumnIndex(LoggDataHelper.LoggDBInfo.COLUMN_LOG_TIME));
+        
+        loggItem.mMsg = cursor.getString(cursor.getColumnIndex(LoggDataHelper.LoggDBInfo.COLUMN_LOG_MSG));
+        return loggItem;
+    }
 
 
 }
